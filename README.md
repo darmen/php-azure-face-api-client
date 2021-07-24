c7b80028ff6e12a2a955c7820a9d561a64bc1828
Darmen Amanbayev <darmen.amanbayev@gmail.com>
2021-07-24 20:15:21 +0200

    Init.

---
+++ README.md
@@ -0,0 +1,1 @@
+# Laravel Azure Face
---
+++ src/AzureFaceServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Darmen\AzureFace;
+
+use Illuminate\Support\ServiceProvider;
+
+class AzureFaceServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
