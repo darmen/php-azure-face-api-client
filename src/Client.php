db29f8c3e86788cb0c76d7410a2e24f99a72c395
Darmen Amanbayev <darmen.amanbayev@gmail.com>
2021-07-25 00:20:07 +0200

    Rename internal client to httpClient for better readabilty

--- src/Client.php
+++ src/Client.php
@@ -9,7 +9,7 @@ class Client
     private const HEADER_SUBSCRIPTION_KEY = 'Ocp-Apim-Subscription-Key';
 
     /** @var HttpClient|null */
-    private $client;
+    private $httpClient;
 
     /** @var array */
     private $httpClientOptions;
@@ -17,33 +17,33 @@ class Client
     /**
      * Client constructor.
      * @param Configuration $configuration Configuration to use
-     * @param HttpClient|null $client Http client to use (defaults to GuzzleHttp)
+     * @param HttpClient|null $httpClient Http client to use (defaults to GuzzleHttp)
      * @param array $httpClientOptions Guzzle client options
      */
-    public function __construct(Configuration $configuration, HttpClient $client = null, array $httpClientOptions = [])
+    public function __construct(Configuration $configuration, HttpClient $httpClient = null, array $httpClientOptions = [])
     {
-        $this->initializeHttpClient($configuration, $client, $httpClientOptions);
+        $this->initializeHttpClient($configuration, $httpClient, $httpClientOptions);
     }
 
     /**
      * Initialize an http client.
      *
      * @param Configuration $configuration
-     * @param HttpClient|null $client
+     * @param HttpClient|null $httpClient
      * @param array $httpClientOptions
      *
      * @return void
      */
-    private function initializeHttpClient(Configuration $configuration, HttpClient $client = null, array $httpClientOptions = []): void
+    private function initializeHttpClient(Configuration $configuration, HttpClient $httpClient = null, array $httpClientOptions = []): void
     {
         $httpClientOptions['base_uri'] = rtrim($configuration->getEndpoint(), '/') . '/face/v1.0/';
         $httpClientOptions['headers'][self::HEADER_SUBSCRIPTION_KEY] = $configuration->getSubscriptionKey();
 
-        if ($client === null) {
-            $client = new HttpClient($httpClientOptions);
+        if ($httpClient === null) {
+            $httpClient = new HttpClient($httpClientOptions);
         }
 
         $this->httpClientOptions = $httpClientOptions;
-        $this->client = $client;
+        $this->httpClient = $httpClient;
     }
 }
