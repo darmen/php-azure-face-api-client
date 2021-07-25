9a349c3170415d0adcadbbad44c7a18a45fab870
Darmen Amanbayev <darmen.amanbayev@gmail.com>
2021-07-25 16:25:34 +0200

    Add LargeFaceList::listFaces method

--- src/Resources/LargeFaceList.php
+++ src/Resources/LargeFaceList.php
@@ -105,6 +105,35 @@ class LargeFaceList extends Resource
     }
 
     /**
+     * List all faces in a large face list, and retrieve face information (including userData and persistedFaceIds of registered faces of the face).
+     *
+     * @see https://docs.microsoft.com/en-us/rest/api/faceapi/large-face-list/list-faces
+     *
+     * @param string $largeFaceListId Id referencing a particular large face list
+     * @param int|null $start Starting face id to return (used to list a range of faces
+     * @param int|null $top Number of faces to return starting with the face id indicated by the 'start' parameter
+     * @return array
+     *
+     * @throws ApiErrorException
+     * @throws GuzzleException
+     */
+    public function listFaces(string $largeFaceListId, int $start = null, int $top = 1000): array
+    {
+        $parameters = [
+            'top' => $top,
+        ];
+
+        if ($start !== null) {
+            $parameters['start'] = $start;
+        }
+
+        return $this->decodeJsonResponse(
+            $this->httpClient->get($this->getUri() . "/$largeFaceListId/persistedfaces?" . http_build_query($parameters))
+        );
+    }
+
+
+    /**
      * Delete a specified large face list.
      *
      * @see https://docs.microsoft.com/en-us/rest/api/faceapi/large-face-list/delete
